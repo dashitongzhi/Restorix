5c0b547c09e04dbde243dda21a935b2a1ae76f1b
dashitongzhi <civilization.cn@outlook.com>
2026-05-15 13:55:01 +0800

    Initial Commit

---
+++ Restorix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restorix
+//
+//  Created by Kral on 2026/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restorix/RestorixApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestorixApp.swift
+//  Restorix
+//
+//  Created by Kral on 2026/5/15.
+//
+
+import SwiftUI
+
+@main
+struct RestorixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
